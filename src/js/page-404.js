2a32dc35fe7429c1567f7d8eb89dfea17e97cf49
XiuhengWu <wuxiuheng79@163.com>
2024-08-13 10:46:17 +0800

    修改 404 页面

--- src/js/page-404.js
+++ src/js/page-404.js
@@ -1,6 +1,21 @@
-var torch = document.querySelector('.torch');
-document.addEventListener('mousemove', function(event) {
-    torch.style.top = event.pageY + 'px';
-    torch.style.left = event.pageX + 'px';
+const url_mapping_table = {
+    '/backrooms-level4294': '/pages/backrooms-level4294/index.html',
+    '/HowToUseV2rayn': '/articles/how-to-use-v2rayn/index.html',
+    '/HowToUseV2rayng': '/articles/how-to-use-v2rayng/index.html',
+    '/html5-flashcard-sets': '/pages/html5-flashcard-sets/index.html',
+    '/page/mamba': '/pages/mamba/index.html'
+};
+const mapped_url = url_mapping_table[location.pathname] || url_mapping_table[location.pathname + '/'] || url_mapping_table[location.pathname + '/index.html'];
+if (mapped_url) {
+    location.replace(mapped_url);
+}
+
+window.addEventListener('load', function() {
+    var torch = document.querySelector('.torch');
+    document.addEventListener('mousemove', function(event) {
+        torch.style.top = event.pageY + 'px';
+        torch.style.left = event.pageX + 'px';
+    });
 });
-console.log(location)
+
+
